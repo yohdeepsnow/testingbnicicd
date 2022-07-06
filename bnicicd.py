35348859679b2774b66f527f86639651193b2eec
yohdeepsnow <yohan.muliono@gmail.com>
2022-07-06 06:43:33 -0700

    tambahin req

--- bnicicd.py
+++ bnicicd.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import datetime
 
 print ("hello world!")
 print ("testing ah nambahin baris")
@@ -6,4 +8,9 @@ print ("mau print apa nih")
 
 res = requests.get("https://www.google.com")
 
-print (res.text)
+os.mkdir("tempResponse")
+
+a=datetime.now()
+
+with open(f"tempResponse/{a}.txt","w") as f:
+	f.write(res.text)
